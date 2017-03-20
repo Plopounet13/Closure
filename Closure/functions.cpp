aeea460a92fbe5539a1735d9d0daf4be83556c97
hmenet <menet.hugo@gmail.com>
2017-03-20 15:35:45 +0100

    Update functions.cpp
    Start of the functions for the class AttSet and FD

--- Closure/functions.cpp
+++ Closure/functions.cpp
@@ -7,3 +7,44 @@
 //
 
 #include "functions.hpp"
+
+AttSet::AttSet(){}
+
+AttSet::AttSet(std::vector<attribute>& initVect){
+	tab.swap(initVect)
+}
+
+AttSet::AttSet(const AttSet& set):tab(set.tab){}
+
+const AttSet& AttSet::operator+=(const AttSet& b){
+	
+	vector<attribute> out;
+	set_union(tab.begin(), tab.end(), b.tab.begin(), b.tab.end(), out.begin());
+	tab.swap(out);
+}
+
+const AttSet& AttSet::operator-=(const AttSet& b){
+	vector<attribute> out;
+	set_difference(tab.begin(), tab.end(), b.tab.begin(), b.tab.end(), out.begin());
+	tab.swap(out);
+}
+
+bool AttSet::isIncluded(const AttSet& b){
+}
+
+
+void unite(const AttSet& a, const AttSet& b, AttSet& out){
+}
+
+void minus(const AttSet& a, const AttSet& b, AttSet& out){
+}
+
+
+static int counter=0;
+
+	int ID;
+	AttSet left;
+	AttSet right;
+
+FD(const AttSet& left, const AttSet right){
+}
