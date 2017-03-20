b2a90f1d8f3fb87497f02c33a473fecaf99c9f64
Plopounet13 <lois.paulin@gmail.com>
2017-03-20 14:06:38 +0100

    Création du projet

---
+++ Closure/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  Closure
+//
+//  Created by Hugo Menet and Loïs Paulin on 20/03/2017.
+//  Copyright © 2017 Hugo Menet and Loïs Paulin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	
+	
+	
+    return 0;
+}
