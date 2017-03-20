39e60860a20acd94e2def46dd760f8c61b1691f3
hmenet <menet.hugo@gmail.com>
2017-03-20 15:20:31 +0100

    Update functions.hpp
    Creation of class AttSet (for sets of attribute) and FD (for functional dependencies).

--- Closure/functions.hpp
+++ Closure/functions.hpp
@@ -11,9 +11,49 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
 
+//attribute
 typedef std::string attribute;
 
+//set of attribute
+class AttSet {
+	std::vector<attribute> tab;
+	
+public:
+	// construct an empty set
+	AttSet();
+	// construct a set from a vector of attribute
+	AttSet(std::vector<attribute>& initVect);
+	// construct as a copy of another set of attribute
+	AttSet(const AttSet& set); 
+	// in place union operator
+	const AttSet& operator+=(const AttSet& b); 
+	// in place minus operator
+	const AttSet& operator-=(const AttSet& b);
+	//in place comparator, check if the set is included in the set b
+	bool isIncluded(const AttSet& b);
+};
 
+//set out to a union b
+void unite(const AttSet& a, const AttSet& b, AttSet& out);
+//set out ot a minus b
+void minus(const AttSet& a, const AttSet& b, AttSet& out);
+
+//functional dependencies
+class FD {
+	//number of FDs already created, to give them a unique identifier
+	static int counter=0;
+public:
+	//identifier of the FD
+	int ID;
+	//functonal dependency : left -> right
+	AttSet left;
+	AttSet right;
+	//construct FD from two sets of attributes
+	FD(const AttSet& left, const AttSet right);
+};
+
+/*We use list of FDs to implement sets of FDs */ 
 
 #endif /* functions_hpp */
