1999c94ac8ad865e6640206535c93a27e0fc4aa4
Plopounet13 <lois.paulin@gmail.com>
2017-03-20 15:22:28 +0100

    Début de gestion des parametres

--- Closure/main.cpp
+++ Closure/main.cpp
@@ -7,23 +7,48 @@
 //
 
 #include <iostream>
+#include <string>
+#include <fstream>
+#include "functions.hpp"
+
+#define NAIVE 1
+#define IMPROVED 2
 
 using namespace std;
 
 void usage(){
-	cerr << "usage: closure -";
+	cerr << "usage: closure option input atts" << endl;
+	
+	cerr << "option list:" << endl;
+	cerr << "-naive: use the naive algorithm" << endl;
+	cerr << "-improved: use the improved algortihm" << endl;
 }
 
-void error(string& msg, int errNum){
-	cerr << endl << msg << endl << endl;
-	usage();
-	exit(errNum);
+void error(const string& msg){
+	cerr << endl << msg << endl;
 }
 
 int main(int argc, const char * argv[]) {
 	
 	if (argc != 4){
-		error("Erreur nombre de parametre")
+		error("Erreur nombre de parametre");
+		usage();
+		cerr << endl;
+		exit(1);
+	}
+	
+	int alg;
+	
+	if (!strcmp(argv[1] ,"-naive")){
+		alg = NAIVE;
+	}else if (!strcmp(argv[1] ,"-improved")){
+		alg = IMPROVED;
+	}
+	
+	ifstream in(argv[2]);
+	if (in.fail()){
+		error("Unable to open input file");
+		exit(2);
 	}
 	
     return 0;
